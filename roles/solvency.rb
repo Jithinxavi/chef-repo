7a5ab2b5a187ee28f2baa356169cba0ac164b181
Thomas Newton <thomas@newtonlabs.org>
2011-03-19 10:21:11 -0400

    changed order of r recipe so bundler can find it

--- roles/solvency.rb
+++ roles/solvency.rb
@@ -3,6 +3,6 @@ description "Solvency generic applciation"
 run_list(
   "recipe[mysql::client]",
   "recipe[apache2]",
-  "recipe[application]",
-  "recipe[r]"
+  "recipe[r]",
+  "recipe[application]"
 )
